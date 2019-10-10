bfd0f112744332c994c8e6908dc3cc67982bfd2a
Synxsaints <Synxsaints@LAPTOP-EAMMNJ87>
2019-10-10 11:19:11 +0700

    First Commit

---
+++ src/kelipatan/Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelipatan;
+
+/**
+ *
+ * @author 
+ * Nama :Permai Ramadhan
+ * NIM  :10118060
+ * Deskripsi Program    : program kelipatan 3,5
+ */
+public class Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i=3.5;
+        while(i<=35){
+            System.out.println(i);
+            i+=3.5;
+       }
+    }
+    
+}
